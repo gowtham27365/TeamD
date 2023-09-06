071a98056b55d114ee14d5fc0d6e6ee0699d5b78
Gowtham kumar <gowtham27365@gmail.com>
2023-09-05 22:42:51 -0700

    ADD Project3 file

---
+++ Project3.sh
@@ -0,0 +1,44 @@
+echo "Enter 1 to view date    | Enter 2 to view time" 
+echo "Enter 3 to view os info | Enter 4 to view load average"   
+
+read x
+
+# Get the current date and time
+
+current_date=$(date +"%A, %B %d, %Y")
+current_time=$(date +"%T")
+
+# Get system load average
+
+load_average=$(cat /proc/loadavg)
+
+# Get OS information
+
+os_info=$(cat /etc/os-release)
+
+if [ $((x)) == 1 ]; then
+    echo current_date:
+
+    echo $current_date
+
+elif [ $((x))  == 2 ]; then 
+    echo current_time:
+
+    echo $current_time
+
+elif [ $((x)) == 3 ]; then 
+
+    echo "OS Information:"
+
+    echo "$os_info"
+
+elif [ $((x)) == 4 ]; then
+    echo load_average:
+
+    echo $load_average
+
+else 
+    echo "Enter correct number to view the correct details"
+
+fi
+
